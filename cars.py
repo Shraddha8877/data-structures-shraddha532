8efbeacf99d461229c24281153ef5bb76347f570
Shraddha Chudal <shraddhachudal@mail.weber.edu>
2025-04-13 21:39:35 -0600

    changec

--- cars.py
+++ cars.py
@@ -1,5 +1,5 @@
 #problem 1. print your first and last name
-print("Shraddha Chudal")
+print("Shradha Chudal")
 #problem 2. In the cars.py create an array named 'cars' with the following elements in this order ---- Ford,Chrysler,Dodge,Ram,Jeep,Chevy,GMC (use single quotes for each element)EX: 'Ford' not "Ford" spelling matters
 cars = ['Ford', 'Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
 #problem 3. print the array to the console
